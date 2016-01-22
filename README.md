5fa88c6dea87d88df16207fa53e72195aec9dc86
AntoineGrn <antoinegrn12@gmail.com>
2016-01-22 08:37:51 +0100

    architecture de symfony

---
+++ README.md
@@ -0,0 +1,4 @@
+IHMProjet
+=========
+
+A Symfony project created on January 21, 2016, 4:27 pm.
---
+++ src/IHMProjet/Bundle/GeneralBundle/IHMProjetGeneralBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace IHMProjet\Bundle\GeneralBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class IHMProjetGeneralBundle extends Bundle
+{
+}

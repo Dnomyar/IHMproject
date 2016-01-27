bbc3ae7301a7dcf8bf8b904c844838c26311cdef
AntoineGrn <antoinegrn12@gmail.com>
2016-01-27 15:08:57 +0100

    suppression de tout les elements

--- README.md
+++
@@ -1,4 +0,0 @@
-IHMProjet
-=========
-
-A Symfony project created on January 21, 2016, 4:27 pm.
--- src/IHMProjet/Bundle/GeneralBundle/IHMProjetGeneralBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace IHMProjet\Bundle\GeneralBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class IHMProjetGeneralBundle extends Bundle
-{
-}
